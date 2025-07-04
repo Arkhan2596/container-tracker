784ea886bc0ec0d8057f8ecaefb7272eb35f5a56
Arkhan2596 <52063764+Arkhan2596@users.noreply.github.com>
2025-07-04 16:13:09 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Playwright browser-ləri quraşdır (lazımlı native deps-lərlə birlikdə)
 python -m playwright install --with-deps
-
-# Flask app-i Gunicorn ilə başlat
 gunicorn app:app --bind 0.0.0.0:10000
